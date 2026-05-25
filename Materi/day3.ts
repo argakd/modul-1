89f9288a30f3b2b0842bd666a223bec739c41dbd
argakd <arga.kd@gmail.com>
2026-05-25 19:27:45 +0700

    add day3 materi and moved gitignore to modul-1

---
+++ Materi/day3.ts
@@ -0,0 +1,16 @@
+// =============== IF STATEMENT ===============
+/*
+    if(condition){
+        code block
+    }
+*/
+
+const age: number = 20;
+
+if (age >=17) {
+    console.log('Anda bisa buat KTP');
+}
+
+// =============== ELSE STATEMENT ===============
+// backup plan dari if statement
+
